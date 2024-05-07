974e024605589c4fb4cddbec708671b824a72716
Praisexodes <130305851+Praisexodes@users.noreply.github.com>
2024-05-07 01:29:28 +0100

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,29 @@
+public class Student extends User {
+    String matric_no = "bhu/22/04/09/0028";
+    String admission_no = "youarecorrect";
+
+    String registerCourse() {
+        if (matric_no.equals("") || admission_no.equals("")) {
+            return "You cannot register your courses at this moment";
+        } else {
+            return "You have registered all courses";
+        }
+    }
+
+    String checkResults() {
+        if (matric_no.equals("") || admission_no.equals("")) {
+            return "Cannot check your result.";
+        } else {
+            return "You got 8As, 2Bs and 1C";
+        }
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Samson Praise";
+        student.password = "PraiseCodes";
+        System.out.println(student.register());
+        System.out.println(student.registerCourse());
+        System.out.println(student.checkResults());
+    }
+}
